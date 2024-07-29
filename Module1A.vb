9e45324574de3ec698c32239173fc190226b14d2
Adriana200413475 <1575171600101@ingenieria.usac.edu.gt>
2024-07-28 19:48:21 -0600

    Add files via upload

---
+++ Module1A.vb
@@ -0,0 +1,16 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim mensaje, nombre As String
+        Console.Clear()
+        Console.WriteLine("Ingrese nombre y No. de Carnet: ")
+        nombre = Console.ReadLine()
+        Console.Clear()
+        mensaje = "Hola a Todo el Mundo !!!!! "
+        Console.WriteLine(mensaje + nombre + " aquì pilas en el 2024 !!!! ")
+        Console.WriteLine("presione cualquier tecla para salir")
+        Console.ReadKey()
+
+    End Sub
+
+End Module
