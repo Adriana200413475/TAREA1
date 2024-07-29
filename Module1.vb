94acb4576c13a3a3e85fcc8db3ea6e162db065be
Adriana200413475 <1575171600101@ingenieria.usac.edu.gt>
2024-07-28 19:44:57 -0600

    Add files via upload

--- Module1.vb
+++ Module1.vb
@@ -1,23 +1,16 @@
 ﻿Module Module1
 
     Sub Main()
-        Dim num1, num2 As Double
-        Console.WriteLine("BIENVENIDO A CALCULADORA EN CONSOLA")
-        Console.WriteLine("-----------------------------------")
-        Console.Write("Por favor Ingresa el 1er Nùmero: ")
-        num1 = Convert.ToDouble(Console.ReadLine())
-        Console.Write("Por favor Ingresa el 2do. Nùmero: ")
-        num2 = Convert.ToDouble(Console.ReadLine())
-        Console.WriteLine("Operaciones Disponibles: ")
-        Console.WriteLine(" +  -> SUMA")
-        Console.WriteLine("El Resultado de la Suma es: " & (num1 + num2))
-        Console.WriteLine(" -  -> RESTA")
-        Console.WriteLine("El Resultado de la Resta es: " & (num1 - num2))
-        Console.WriteLine(" *  -> MULTIPLICACIÓN")
-        Console.WriteLine("El Resultado de la Multiplicación: " & (num1 * num2))
-        Console.WriteLine(" /  -> DIVISIÓN")
-        Console.WriteLine("El Resultado de la Divisiòn es: " & (num1 / num2))
-        Console.WriteLine("Presiona cualquier tecla para salir...")
+        Dim mensaje, nombre As String
+        Console.Clear()
+        Console.WriteLine("Ingrese nombre y No. de Carnet: ")
+        nombre = Console.ReadLine()
+        Console.Clear()
+        mensaje = "Hola a Todo el Mundo !!!!! "
+        Console.WriteLine(mensaje + nombre + " aquì pilas en el 2024 !!!! ")
+        Console.WriteLine("presione cualquier tecla para salir")
         Console.ReadKey()
+
     End Sub
+
 End Module
