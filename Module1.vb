7388936fa6b73d826d7fbcfe74d71b5d46244c7b
Adriana200413475 <1575171600101@ingenieria.usac.edu.gt>
2024-07-28 19:39:46 -0600

    Add files via upload

---
+++ Module1.vb
@@ -0,0 +1,23 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim num1, num2 As Double
+        Console.WriteLine("BIENVENIDO A CALCULADORA EN CONSOLA")
+        Console.WriteLine("-----------------------------------")
+        Console.Write("Por favor Ingresa el 1er Nùmero: ")
+        num1 = Convert.ToDouble(Console.ReadLine())
+        Console.Write("Por favor Ingresa el 2do. Nùmero: ")
+        num2 = Convert.ToDouble(Console.ReadLine())
+        Console.WriteLine("Operaciones Disponibles: ")
+        Console.WriteLine(" +  -> SUMA")
+        Console.WriteLine("El Resultado de la Suma es: " & (num1 + num2))
+        Console.WriteLine(" -  -> RESTA")
+        Console.WriteLine("El Resultado de la Resta es: " & (num1 - num2))
+        Console.WriteLine(" *  -> MULTIPLICACIÓN")
+        Console.WriteLine("El Resultado de la Multiplicación: " & (num1 * num2))
+        Console.WriteLine(" /  -> DIVISIÓN")
+        Console.WriteLine("El Resultado de la Divisiòn es: " & (num1 / num2))
+        Console.WriteLine("Presiona cualquier tecla para salir...")
+        Console.ReadKey()
+    End Sub
+End Module
